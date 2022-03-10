e3196bc9178577412340251586be61d9dd3e3d99
xiaopeng <1637274818@qq.com>
2022-03-10 11:36:20 +0800

    update OOP特性

--- c++/code_instances/inheritance_and_priority.cpp
+++ c++/code_instances/inheritance_and_priority.cpp
@@ -22,6 +22,10 @@
 派生类对象 都含有 基类对象。
 
 
+    三个作用域：类内、类的派生类、类的用户
+    类内可以自由访问，类内的 权限 指明了类的派生类、类的用户对类内的访问权限
+    类的继承类型（派生访问说明符）指明了，派生类的派生类、派生类的用户对基类（继承来的权限）的访问权限，相当于一个buff叠加，它（派生访问说明符）只负责对继承过来的成员的权限的管理（一种叠加管理）
+
 */
 
 
@@ -51,7 +55,7 @@ private:
 
 };
 
-class Special_s : public Student {
+class Special_s : protected Student {
 
 public:
 
@@ -65,8 +69,43 @@ public:
         cout << this->get_grade() << endl;
     }
 
+    void func(int j) {
+        cout << j << endl;
+    }
+
 protected:
     int is_disordered;
+
+};
+
+class Sspecial_s : public Special_s {
+public:
+    Sspecial_s(int arg1, int arg2, int arg3, int arg4) : Special_s(arg1, arg2, arg3), is_boy(arg4) {}
+
+    // 这么写 应该就递归了，因为编译器应该首先找到的是自己的get_grade()，基类的
+    // int get_grade() { return this->get_grade(); }
+    int _get_grade() { return this->get_grade(); }
+    int _get_number() { return this->get_number(); }
+
+
+    // 覆盖了基类中的func()，从“名字查找先于类型检查”、“先内层，在外层”来看，在内层找到fun，却和func(arg)类型不匹配，所以为了使用基类的其他重载的func，必须使用using声明，
+    // 一次性全部引入，或者全部重写（覆盖）
+    using Special_s::func;
+    void func() {
+        cout << 200 << endl;
+    }
+    
+
+private:
+    int is_boy;
+
+};
+
+class A : private Student {
+public:
+    // 可以使用 using 改变继承而来的可访问的权限（可改变基类的非私有成员），如：基类的public、protected；而对基类的private，本就不具备访问权限，无法改变其在派生类的权限
+    // 会报错
+    // using Student::grade;
 
 };
 
@@ -75,15 +114,24 @@ int main(int argc, char **argv) {
 
     Student s(9, 2);
     Special_s ss(10, 5, 0);
+    Sspecial_s sss(15, 3, 1, 1);
 
     cout << s.get_grade() << endl;
     cout << s.get_number() << endl;
 
-    cout << ss.get_grade() << endl;
-    cout << ss.get_number() << endl;
+    
     cout << ss.get_is_disordered() << endl;
 
     ss.func();
+    sss.func(100);
+    sss.func();
+
+    // Special_s protected继承了Student，所以Special_s的用户不能访问Student的方法，但是Special_s的派生类可以访问Special_s的public方法
+    // 这里的SSpecial_s封装了对他来说是protected的Student的get_grade方法，封装成public，所以SSpecial_s的用户可以访问。
+    // cout << ss.get_grade() << endl;
+    // cout << ss.get_number() << endl;
+    cout << sss._get_grade() << endl;
+    cout << sss._get_number() << endl;
 
     cout << sizeof(Student) << endl;
     cout << sizeof(Special_s) << endl;
